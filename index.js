12b731fdaf5ea1ae2c717b9d429b56baab01d379
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-09 20:53:05 +0300

    create function to overwrite global variable

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 var customerName = 'bob';
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+ function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+ }
